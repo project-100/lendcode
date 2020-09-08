72e1d161eaa53b5f3844a0707dd0ff15ae81c264
aravinthraj <aravinth799902@gmail.com>
2020-09-08 19:37:02 +0530

    added keyboard and mouse event instead of intervel

--- public/js/senddata.js
+++ public/js/senddata.js
@@ -14,13 +14,30 @@ socket.on("senddata",message=>{
 
 socket.on("code",message=>{
 document.getElementById("text").value=message;
+
+});
+document.getElementById('text').addEventListener( "mousedown", ()=>{
+  var data=document.getElementById('text').value;
+  var messages = {room,data}
+  console.log("mouseame");
+  if(data!=get){
+    socket.emit("code",messages);
+    get=data;
+    console.log("not same");
+}
+
+});
+document.getElementById('text').addEventListener( "keypress", ()=>{
+  var data=document.getElementById('text').value;
+  console.log("mouseaddme");
+  var messages = {room,data}
+  if(data!=get){
+    socket.emit("code",messages);
+    get=data;
+    console.log("not same");
+}
 });
 
-setInterval(()=>{
-var data=document.getElementById('text').value;
-var messages={room,data};
-if(data!=get){
-    socket.emit("code",messages);
-}
-get=data;
-},4000);
+
+
+
