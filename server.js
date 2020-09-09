e6bb3116fccc0b423e279ee21a2cc6ae0f7078e6
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-09 18:06:21 +0530

    New Changes

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+
+const { port } = require('./env');
+
 const path = require('path');
-const port = process.env.PORT || 3000;
+
 const http = require('http');
 const server = http.createServer(app);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const socketio = require('socket.io');
 const io = socketio(server);
-io.on('connection',socket =>{
-    console.log('connected');
-socket.on("room",message=>{
+
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('room', (message) => {
     socket.join(message);
-    socket.broadcast.to(message).emit("senddata","userjoined");
-});
-socket.on("code",message=>{
-     socket.broadcast.to(message.room).emit("code",message.data);
+    socket.broadcast.to(message).emit('senddata', 'userjoined');
+  });
+  socket.on('code', (message) => {
+    socket.broadcast.to(message.room).emit('code', message.data);
+  });
 });
 
-});
-
-server.listen(port,()=>{
-    console.log(`server running at ->http://localhost/${port}`)
+server.listen(port, () => {
+  console.log(`Server running at -> http://localhost/${port}`);
 });
